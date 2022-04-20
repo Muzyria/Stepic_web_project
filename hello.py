7e4def38dee66ec32eaed32a7f4491dd63689506
“Muzyria” <fila090580@gmail.com>
2022-04-20 14:54:10 +0300

    n

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
-def application(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    body = [i + '\n' for i in environ['QUERY_STRING'].split('&')]
-
-    start_response(status, headers)
-    return body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
